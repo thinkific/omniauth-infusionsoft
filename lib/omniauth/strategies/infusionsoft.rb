9be4c4a432c49c0f6b45956d40732e102b4b6bd1
Ben Schock <schock.ben@googlemail.com>
2015-10-29 17:45:14 -0700

    added https override

--- lib/omniauth/strategies/infusionsoft.rb
+++ lib/omniauth/strategies/infusionsoft.rb
@@ -11,6 +11,22 @@ module OmniAuth
         site:          'https://signin.infusionsoft.com'
       }
 
+      def full_host
+        case OmniAuth.config.full_host
+          when String
+            OmniAuth.config.full_host
+          when Proc
+            OmniAuth.config.full_host.call(env)
+          else
+            uri = URI.parse(request.url.gsub(/\?.*$/,''))
+            uri.path = ''
+            uri.query = nil
+            #sometimes the url is actually showing http inside rails because the other layers (like nginx) have handled the ssl termination.
+            uri.scheme = 'https'
+            uri.to_s
+        end
+      end
+
     end
   end
 end
